773a375ef83b4c22750a79bcebfb21e958de8b8b
radovanbotik <radovanbotik@gmail.com>
2022-09-16 23:49:53 +0200

    position sticky

---
+++ index.js
@@ -0,0 +1,17 @@
+// const header = document.querySelector("header");
+// const firstSection = document.querySelector("section:nth-of-type(1)");
+
+// const options = {
+//   threshold: 0,
+// };
+// const adjustHeader = (entries, observer) => {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       entry.target.style.position = "fixed";
+//       entry.target.style.top = 0;
+//     }
+//   });
+// };
+
+// const headerObserver = new IntersectionObserver(adjustHeader, options);
+// headerObserver.observe(header);
