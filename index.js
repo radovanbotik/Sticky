e5374f4dc2f9edf8e6afebfa9e30b86a7dfd686c
radovanbotik <radovanbotik@gmail.com>
2022-09-17 14:29:53 +0200

    hide on scroll-down / appear on scroll-up

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-// const header = document.querySelector("header");
-// const firstSection = document.querySelector("section:nth-of-type(1)");
+const header = document.querySelector("header");
 
-// const options = {
-//   threshold: 0,
-// };
-// const adjustHeader = (entries, observer) => {
-//   entries.forEach(entry => {
-//     if (!entry.isIntersecting) {
-//       entry.target.style.position = "fixed";
-//       entry.target.style.top = 0;
-//     }
-//   });
-// };
+let lastScroll = window.pageYOffset;
 
-// const headerObserver = new IntersectionObserver(adjustHeader, options);
-// headerObserver.observe(header);
+const getDimensions = () => {
+  const headerTop = header.getBoundingClientRect().top;
+  const headerBottom = header.getBoundingClientRect().bottom;
+  const headerHeight = headerBottom - headerTop;
+  return headerHeight;
+};
+
+const adjustHeader = () => {};
+
+window.addEventListener("scroll", e => {
+  let currentScroll = e.currentTarget.pageYOffset;
+
+  if (currentScroll > lastScroll) {
+    header.style.top = `-${getDimensions()}px`;
+    //update the value
+    lastScroll = currentScroll;
+  } else {
+    header.style.top = "0px";
+    lastScroll = currentScroll;
+  }
+});
